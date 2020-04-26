6b7f397923269d593f00cc50317e412599af92c8
John Diss <jaundice@Mac-Pro.local>
2020-04-26 02:15:44 +0100

    kinda works

--- platform/minilogue-xd/demos/wavefolder/wavefolder.cpp
+++ platform/minilogue-xd/demos/wavefolder/wavefolder.cpp
@@ -45,6 +45,33 @@ float MinLimit;
 float Aggression;
 float Gain;
 
+// float powf_fast(float a, float b)
+// {
+//   union {
+//     float d;
+//     int x;
+//   } u = {a};
+//   u.x = (int)(b * (u.x - 1064866805) + 1064866805);
+//   return u.d;
+// }
+
+float Delta(float delta)
+{
+  return fastpowf(delta, Aggression);
+}
+
+inline float Fold(float s)
+{
+  if (!(s > MinLimit && s < MaxLimit))
+  {
+    do
+    {
+      s = s > MaxLimit ? MaxLimit - Delta(s - MaxLimit) : MinLimit + Delta(MinLimit - s);
+    } while (s > MaxLimit || s < MinLimit);
+  }
+  return s;
+}
+
 void MODFX_INIT(uint32_t platform, uint32_t api)
 {
   (void)platform;
@@ -58,25 +85,28 @@ void MODFX_INIT(uint32_t platform, uint32_t api)
 
 void MODFX_PROCESS(const float *main_xn, float *main_yn, const float *sub_xn, float *sub_yn, uint32_t frames)
 {
-  q31_t *__restrict y = (q31_t *)main_yn;
-  const q31_t *y_e = y + frames;
+  const float *mx = main_xn;
+  float *__restrict my = main_yn;
+  const float *my_e = my + 2 * frames;
 
-  for (; y != y_e;)
+  const float *sx = sub_xn;
+  float *__restrict sy = sub_yn;
+
+  // Loop through the samples
+  for (; my != my_e;)
   {
+    // Pass sub through for prologue for now (L,R), you will need a separate filter at least if you want to process this properly
+    *(sy++) = *(sx++); // Copy *sy to *sx (Left channel)
+    *(sy++) = *(sx++); // Copy *sy to *sx (Right channel)
 
-    float s = (*(++main_xn));
+    float s = Gain * (*mx++);
+    float s2 = Gain * (*mx++);
 
-    if (!(s >= MinLimit && s <= MaxLimit))
-    {
-      do
-      {
-        s = s > MaxLimit? MaxLimit - fastpowf(s - MaxLimit, Aggression) : MinLimit + fastpowf(MinLimit - s, Aggression);
-      } while (s > MaxLimit || s < MinLimit);
-    }
+    ///s = Gain * s;
+    float invGain = 1.f / Gain;
 
-    s = Gain * s;
-
-    *(++main_yn) = s;
+    *(my++) = fmax(-1, fmin(1,  invGain * Fold(s)));
+    *(my++) = fmax(-1, fmin(1, invGain * Fold(s2)));
   }
 }
 
@@ -86,17 +116,21 @@ void MODFX_PARAM(uint8_t index, int32_t value)
 
   switch (index)
   {
-    case 1:
-    {
-      MaxLimit = val;
-      MinLimit = -1.f * val;
-      Gain = 1.f / val;
-      break;
-    }
-    case 2:
-    {
-      Aggression = val;
-      break;
-    }
+  case 0:
+  {
+    val *= 0.9f;
+    val = 1.f - val;
+    MaxLimit = val;
+    MinLimit = -1.f * val;
+    Gain = 1.f / val;
+    break;
+  }
+  case 1:
+  {
+    val *= 0.9;
+    val = 1.f - val;
+    Aggression = 0.2f + 5 * val;
+    break;
+  }
   }
 }
